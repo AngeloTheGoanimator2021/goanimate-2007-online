a9207bb789324ca2a1ac989857ee84c48c408b9c
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-04-11 00:36:58 -0400

    Update page.js

--- static/page.js
+++ static/page.js
@@ -77,7 +77,7 @@ module.exports = function (req, res, url) {
 		case "/player": {
 			title = "Player";
 			attrs = {
-				data: process.env.SWF_URL + "/player.swf",
+				data: process.env.SWF_URL + "/rssplayer.swf",
 				type: "application/x-shockwave-flash",
 				width: "100%",
 				height: "100%",
